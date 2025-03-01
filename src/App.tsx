8acb1df104fa5ea5f65c60a149f870bb08f83ff0
hydrocaeris <nurfatihsatria@gmail.com>
2025-03-01 12:06:26 +0800

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,14 @@
+import './App.css'
+
+function App() {
+  return (
+    <>
+      <div>
+        <h1>Hello World!</h1>
+        <h4>Ini My Vite :3</h4>
+      </div>
+    </>
+  )
+}
+
+export default App
